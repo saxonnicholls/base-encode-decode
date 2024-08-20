33a51fde1f433c920c39d1ed0cda775a538165bd
Saxon Nicholls <me@saxonnicholls.com>
2024-08-20 17:21:56 +1000

    Initial Commit

---
+++ BaseEncodeDecode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BaseEncodeDecode
+//
+//  Created by Saxon Nicholls on 20/8/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
