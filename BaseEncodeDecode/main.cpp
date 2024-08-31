9d3fdd72c815d77797a62779c6c06d6b4f76d4c2
Saxon Nicholls <me@saxonnicholls.com>
2024-08-31 16:50:11 +1000

    fixed order in main() added Base4 example

--- BaseEncodeDecode/main.cpp
+++ BaseEncodeDecode/main.cpp
@@ -22,6 +22,12 @@ int main() {
     std::cout << "Base2 Encoded (Serial): " << encodedBase2 << std::endl;
     std::cout << "Base2 Decoded (Serial): " << decodedBase2 << std::endl;
 
+    std::string encodedBase4 = EncodeBase4(data);
+    std::string decodedBase4 = DecodeBase4(encodedBase4);
+
+    std::cout << "Base4 Encoded (Serial): " << encodedBase4 << std::endl;
+    std::cout << "Base4 Decoded (Serial): " << decodedBase4 << std::endl;
+    
     // Base8 Encoding/Decoding
     std::string encodedBase8 = EncodeBase8(data);
     std::string decodedBase8 = DecodeBase8(encodedBase8);
@@ -29,12 +35,12 @@ int main() {
     std::cout << "Base8 Encoded (Serial): " << encodedBase8 << std::endl;
     std::cout << "Base8 Decoded (Serial): " << decodedBase8 << std::endl;
 
-    // Base64 Encoding/Decoding
-    std::string encodedBase64 = EncodeBase64(data);
-    std::string decodedBase64 = DecodeBase64(encodedBase64);
- 
-    std::cout << "Base64 Encoded (Serial): " << encodedBase64 << std::endl;
-    std::cout << "Base64 Decoded (Serial): " << decodedBase64 << std::endl;
+    // Base16 Encoding/Decoding
+    std::string encodedBase16 = EncodeBase16(data);
+    std::string decodedBase16 = DecodeBase16(encodedBase16);
+
+    std::cout << "Base16 Encoded (Serial): " << encodedBase16 << std::endl;
+    std::cout << "Base16 Decoded (Serial): " << decodedBase16 << std::endl;
 
     // Base32 Encoding/Decoding
     std::string encodedBase32 = EncodeBase32(data);
@@ -50,13 +56,12 @@ int main() {
     std::cout << "Base32Hex Encoded (Serial): " << encodedBase32Hex << std::endl;
     std::cout << "Base32Hex Decoded (Serial): " << decodedBase32Hex << std::endl;
 
-    // Base16 Encoding/Decoding
-    std::string encodedBase16 = EncodeBase16(data);
-    std::string decodedBase16 = DecodeBase16(encodedBase16);
-
-    std::cout << "Base16 Encoded (Serial): " << encodedBase16 << std::endl;
-    std::cout << "Base16 Decoded (Serial): " << decodedBase16 << std::endl;
-
+    // Base64 Encoding/Decoding
+    std::string encodedBase64 = EncodeBase64(data);
+    std::string decodedBase64 = DecodeBase64(encodedBase64);
+ 
+    std::cout << "Base64 Encoded (Serial): " << encodedBase64 << std::endl;
+    std::cout << "Base64 Decoded (Serial): " << decodedBase64 << std::endl;
     return 0;
 }
 
